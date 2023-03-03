f237df1a16a39105e96369093b9c7b1d3c302ef7
nikolaivanovski <ivanovskinicola@gmail.com>
2023-03-03 15:50:31 +0100

    user login

---
+++ Homework_UserLogin/app.js
@@ -0,0 +1,30 @@
+﻿const colors = require("colors");
+
+let users = [
+  {
+    role: "admin",
+    fullName: "John Doe",
+    username: "qwerty",
+    password: "123qwe",
+  },
+  {
+    role: "client",
+    fullName: "Bob Bobski",
+    username: "asdasd",
+    password: "asdzxdfx",
+  },
+];
+
+const loggedIn = (username, password) => {
+  for (const user of users) {
+    if (user.username === username && user.password === password) {
+      console.log("User is logged in".green);
+    } else {
+      console.log("User not found".red);
+    }
+  }
+};
+
+loggedIn("qwerty", "123qwe");
+loggedIn("asdasd", "sadqwrfa");
+loggedIn("afasfw", "fqfwfasd");
