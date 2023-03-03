39a3f8a8dbb9308ec546ba1705c85580766c02e4
nikolaivanovski <ivanovskinicola@gmail.com>
2023-03-03 22:04:23 +0100

    homework 2

---
+++ Homework_2/index.js
@@ -0,0 +1,12 @@
+﻿const fs = require("fs");
+const path = require("path");
+
+const pathToHomeworkFile = path.join(__dirname, "homework.txt");
+
+fs.writeFileSync(pathToHomeworkFile, "Homework 02 in Basic Node", (err) => {
+  if (err) throw err;
+});
+
+fs.appendFileSync(pathToHomeworkFile, "\nFINISHED!");
+const content = fs.readFileSync(pathToHomeworkFile, { encoding: "utf-8" });
+console.log(content);
