368205511b3fe80549cb4316fed4aa5c4c492760
nikolaivanovski <ivanovskinicola@gmail.com>
2023-03-06 18:49:49 +0100

    class-04

---
+++ Class_04/event-service.js
@@ -0,0 +1,19 @@
+﻿import { EventEmitter } from "events";
+import fileService from "./file-service.js";
+
+const emmiter = new EventEmitter();
+
+emmiter.on("movie_added", (movieName) => {
+  console.log("Movie added event...");
+
+  const currentTime = new Date().toLocaleString();
+
+  const logMessage = `
+    Movie with name ${movieName} was added at: ${currentTime}
+    ________________________________________________________  
+    `;
+
+  fileService.appendToFile("./db/add_movie_logs.txt", logMessage);
+});
+
+export default emmiter;
---
+++ Class_04/file-service.js
@@ -0,0 +1,25 @@
+﻿import fs from "fs";
+
+const writeToFile = (path, data) => {
+  fs.writeFileSync(path, data);
+};
+
+const readFromFile = (path) => {
+  console.log("1");
+  const content = fs.readFileSync(path, { encoding: "utf-8" });
+  console.log(typeof content);
+  const parsedContent = JSON.parse(content);
+  console.log(parsedContent);
+  console.log("2");
+  return content;
+};
+
+const appendToFile = (path, data) => {
+  fs.appendFileSync(path, data);
+};
+
+export default {
+  writeToFile,
+  readFromFile,
+  appendToFile,
+};
